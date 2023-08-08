d8fa38ae44f3069eaebb04d308fac755d56f7113
Jobayer <jobayerhossain977@gmail.com>
2023-08-08 13:39:55 +0600

    main: Fixed

--- backend/server.js
+++ backend/server.js
@@ -7,33 +7,31 @@ const cors = require('cors');
 // Enable CORS
 app.use(cors());
 
-// Serve article data
-const articles = [
-	{
-		id: 1,
-		title: 'Article 1',
-		content: 'This is the content of Article 1.',
-	},
-	{
-		id: 2,
-		title: 'Article 2',
-		content: 'This is the content of Article 2.',
-	},
-];
+app.get('/articles', (req, res) => {
+	const time = new Date().toLocaleTimeString('en-US', {
+		second: 'numeric',
+		minute: 'numeric',
+		hour: 'numeric',
+		calendar: 'gregory',
+	});
+	console.log(`${req.url} Gets called at ${time}`);
 
-app.get('/articles/:id', (req, res) => {
-	console.log(`${req.url} Gets called at ${Date.now()}`);
-	const articleId = parseInt(req.params.id);
-	const article = articles.find((article) => article.id === articleId);
-	if (!article) {
-		return res.status(404).json({ error: 'Article not found' });
-	}
+	// res.set('Expires', new Date(Date.now() + 15 * 1000));
+	// res.set('Pragma', 'no-cache');
 
-	// Set caching headers
-
-	res.setHeader('Cache-Control', 'no-cache');
-
-	res.json(article);
+	res.set('Cache-Control', 'public, max-age=1115, must-revalidate');
+	res.json([
+		{
+			id: 1,
+			title: 'Article 1',
+			content: 'This is the content of Article 1.',
+		},
+		{
+			id: 2,
+			title: 'Article 2',
+			content: 'This is the content of Article 2.',
+		},
+	]);
 });
 
 app.listen(4000, () => {
--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,36 +1,40 @@
 import React, { useEffect, useState } from 'react';
-
 function App() {
-	const [article, setArticle] = useState(null);
-
+	const [articles, setArticles] = useState(null);
+	const [headers, setHeaders] = useState(null);
 	useEffect(() => {
 		const fetchData = async () => {
-			try {
-				const querying = new URLSearchParams(window.location.search).get('id');
-				const response = await fetch(
-					`http://localhost:4000/articles/${querying}`
-				);
-				const data = await response.json();
-				setArticle(data);
-				console.log('Hello');
-			} catch (error) {
-				console.error(error);
-			}
+			const response = await fetch(`http://localhost:4000/articles`);
+			const data = await response.json();
+			const headers = [];
+			response.headers.forEach((value, key) => {
+				headers.push(`${key}: ${value}`);
+			});
+			setHeaders(headers);
+			setArticles(data);
 		};
-
 		fetchData();
-	}, []); // Empty dependency array removed
-
-	if (!article) {
+	}, []);
+	if (!articles) {
 		return <p>Loading...</p>;
 	}
-
 	return (
 		<div>
-			<h2>{article.title}</h2>
-			<p>{article.content}</p>
+			<h2>Headers</h2>
+			<ul>
+				{headers?.map((value, key) => {
+					return <li>{value}</li>;
+				})}
+			</ul>
+			<br />
+			<h2>Response</h2>
+			{articles.map((article) => (
+				<>
+					<h2>{article.title}</h2>
+					<p>{article.content}</p>
+				</>
+			))}
 		</div>
 	);
 }
-
 export default App;
